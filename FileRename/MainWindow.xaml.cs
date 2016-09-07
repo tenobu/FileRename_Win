46a20dfe75304268b9b8ba16fa61f18d8ba38ad7
tenobu <tenobu@gmail.com>
2016-09-07 16:39:07 +0900

    2016/09/07 16:40:00 TreeView

--- FileRename/MainWindow.xaml.cs
+++ FileRename/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
 using win = System.Windows.Forms;
+using System.Collections.ObjectModel;
 
 namespace FileRename
 {
@@ -28,10 +29,14 @@ namespace FileRename
 
 		private long long_FolderCount = 0, long_FileCount, long_Length = 0;
 
+		private Folder_data folders_Data = null;
+
 		private List<FolderFile_data> list_FolderFiles = null;
 		//private bool bool_ListFlag = false;
 
 		private Dictionary<string, List<FolderFile_data>> dic_FolderFiles = null;
+
+		//private System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer(); 
 
 
 		public MainWindow()
@@ -115,8 +120,6 @@ namespace FileRename
 				label_Folder.Content = files[0];
 				str_Folder = (string)label_Folder.Content;
 
-				long_FolderCount = long_FileCount = long_Length = 0;
-
 				CheckFolder();
 			}
 		}
@@ -168,26 +171,52 @@ namespace FileRename
 			CheckFolder();
 		}
 
+		/*private void dispatcherTimer_Tick(object sender, EventArgs e)
+		{
+			if (list_FolderFiles != null)
+			{
+				//listBox_FolderFiles.ItemsSource = list_FolderFiles;
+				listBox_FolderFiles.Items.Add(list_FolderFiles);
+			}
+		}*/
+		
 		private void CheckFolder()
 		{
+			folders_Data = new Folder_data(str_Folder);
+
 			list_FolderFiles = null;
 			dic_FolderFiles  = new Dictionary<string, List<FolderFile_data>>();
 
-			listBox_FolderFiles.Items.Clear();
+			//listBox_FolderFiles.Items.Clear();
 
 			long_FolderCount = long_FileCount = long_Length = 0;
 
 			//bool_ListFlag = false;
 
+			/*timer.Tick += new EventHandler(dispatcherTimer_Tick);
+			timer.Interval = new TimeSpan(0, 0, 1);
+			timer.Start();*/
+
 			// タスクを分けて、メインからサブにタスクを移す。
-			var task = Task.Factory.StartNew(() =>
-			{
-				GetFolder(str_Folder);
-			});
+			//var task = Task.Factory.StartNew(() =>
+			//{
+				GetFolder(str_Folder, folders_Data);
+
+				treeView_Folder.ItemsSource = null;
+				treeView_Folder.ItemsSource = folders_Data;
+			
+			//timer.Stop();
+			//});
 		}
 
-		private void GetFolder(string folder_path)
+		private void GetFolder(string folder_path, Folder_data fd)
 		{
+			var di = new DirectoryInfo(folder_path);
+
+			fd.image_Icon  = new BitmapImage(new Uri(@"Images\folder.png", UriKind.Relative));
+			fd.str_FPath   = folder_path;
+			fd.str_FName   = new DirectoryInfo(folder_path).Name;
+	
 			var list_ff = new List<FolderFile_data>();
 
 			dic_FolderFiles.Add(folder_path, list_ff);
@@ -205,6 +234,32 @@ namespace FileRename
 				bool_ListFlag = false;
 			}
 
+			var ch = fd.fd_Children;
+
+			foreach (var c_folder_path in Directory.GetDirectories(folder_path))
+			{
+				var ch_a = new Folder_data(c_folder_path);
+
+				treeView_Folder.ItemsSource = null;
+				treeView_Folder.ItemsSource = folders_Data;
+
+				AddFolderFilesList(list_ff, bool_ListFlag, "Directory", c_folder_path);
+
+				long_FolderCount++;
+
+				button_Check.Dispatcher.BeginInvoke(
+					new Action(() =>
+					{
+						label_Count.Content =
+							long_FolderCount.ToString("フォルダ数 ###,##0") + "、" +
+							long_FileCount  .ToString("ファイル数 ###,##0");
+					}));
+
+				GetFolder(c_folder_path, ch_a);
+
+				ch.Add(ch_a);
+			}
+
 			foreach (var file_path in Directory.GetFiles(folder_path))
 			{
 				AddFolderFilesList(list_ff, bool_ListFlag, "File", file_path);
@@ -218,95 +273,74 @@ namespace FileRename
 						label_Count.Content =
 							long_FolderCount.ToString("フォルダ数 ###,##0") + "、" +
 							long_FileCount  .ToString("ファイル数 ###,##0");
-						label_Size.Content =
+						label_Size .Content =
 							long_Length     .ToString("サイズ ###,##0 Byte");
 					}));
-			}
-
-			foreach (var c_folder_path in Directory.GetDirectories(folder_path))
-			{
-				AddFolderFilesList(list_ff, bool_ListFlag, "Directory", c_folder_path);
-
-				long_FolderCount++;
-
-				button_Check.Dispatcher.BeginInvoke(
-					new Action(() =>
-					{
-						label_Count.Content =
-							long_FolderCount.ToString("フォルダ数 ###,##0") + "、" +
-							long_FileCount  .ToString("ファイル数 ###,##0");
-					}));
-
-				GetFolder(c_folder_path);
 			}
 		}
 
 		private void AddFolderFilesList(List<FolderFile_data> list_ff, bool bool_ListFlag, string str_ff, string str_path)
 		{
-			//listBox_FolderFiles.Dispatcher.BeginInvoke(
-			//	new Action(() =>
-			//	{
-					var ffd = new FolderFile_data();
+			var ffd = new FolderFile_data();
 
-					ffd.str_Type   = str_ff;
-					ffd.str_FFPath = str_path;
+			ffd.str_Type = str_ff;
+			ffd.str_FFPath = str_path;
 
-					if (str_ff.Equals("Directory"))
-					{
-						var di = new DirectoryInfo(str_path);
+			if (str_ff.Equals("Directory"))
+			{
+				var di = new DirectoryInfo(str_path);
 
-						ffd.image_Icon = new BitmapImage(new Uri(@"Images\folder.png", UriKind.Relative));
-						ffd.str_FFName = di.Name;
-						ffd.long_Size  = 0;
-						ffd.str_SizeS  = "";
+				ffd.image_Icon = new BitmapImage(new Uri(@"Images\folder.png", UriKind.Relative));
+				ffd.str_FFName = di.Name;
+				ffd.long_Size = 0;
+				ffd.str_SizeS = "";
 
-						list_ff.Add(ffd);
+				list_ff.Add(ffd);
 
-						if (bool_ListFlag)
+				if (bool_ListFlag)
+				{
+					listBox_FolderFiles.Dispatcher.BeginInvoke(
+						new Action(() =>
 						{
-							listBox_FolderFiles.Dispatcher.BeginInvoke(
-								new Action(() =>
-								{
-									listBox_FolderFiles.ItemsSource = list_ff;
-								}));
-						}
-					}
-					else if (
-						str_ff.Equals("File"))
-					{
-						var fi = new FileInfo(str_path);
+							listBox_FolderFiles.ItemsSource = list_ff;
+						}));
+				}
+			}
+			else if (
+				str_ff.Equals("File"))
+			{
+				var fi = new FileInfo(str_path);
 
-						if (fi.Extension.ToLower().Equals(".bmp" ) ||
-							fi.Extension.ToLower().Equals(".gif" ) ||
-							fi.Extension.ToLower().Equals(".jpg" ) ||
-							fi.Extension.ToLower().Equals(".jpeg") ||
-							fi.Extension.ToLower().Equals(".png" ) ||
-							fi.Extension.ToLower().Equals(".tiff") ||
-							fi.Extension.ToLower().Equals(".ico" ))
+				if (fi.Extension.ToLower().Equals(".bmp" ) ||
+					fi.Extension.ToLower().Equals(".gif" ) ||
+					fi.Extension.ToLower().Equals(".jpg" ) ||
+					fi.Extension.ToLower().Equals(".jpeg") ||
+					fi.Extension.ToLower().Equals(".png" ) ||
+					fi.Extension.ToLower().Equals(".tiff") ||
+					fi.Extension.ToLower().Equals(".ico" ))
+				{
+					ffd.image_Icon = new BitmapImage(new Uri(fi.FullName));
+				}
+				else
+				{
+					ffd.image_Icon = SHFileInfo.GetBitmap(fi.FullName);
+				}
+
+				ffd.str_FFName = fi.Name;
+				ffd.long_Size = fi.Length;
+				ffd.str_SizeS = fi.Length.ToString("###,##0 Byte");
+
+				list_ff.Add(ffd);
+
+				if (bool_ListFlag)
+				{
+					listBox_FolderFiles.Dispatcher.BeginInvoke(
+						new Action(() =>
 						{
-							ffd.image_Icon = new BitmapImage(new Uri(fi.FullName));
-						}
-						else
-						{
-							ffd.image_Icon = SHFileInfo.GetBitmap(fi.FullName);
-						}
-
-						ffd.str_FFName = fi.Name;
-						ffd.long_Size  = fi.Length;
-						ffd.str_SizeS  = fi.Length.ToString("###,##0 Byte");
-
-						list_ff.Add(ffd);
-
-						if (bool_ListFlag)
-						{
-							listBox_FolderFiles.Dispatcher.BeginInvoke(
-								new Action(() =>
-								{
-									listBox_FolderFiles.ItemsSource = list_ff;
-								}));
-						}
-					}
-				//}));
+							listBox_FolderFiles.ItemsSource = list_ff;
+						}));
+				}
+			}
 		}
 
 		private void listBox_FolderFiles_MouseUp(object sender, MouseButtonEventArgs e)
@@ -318,7 +352,43 @@ namespace FileRename
 		{
 
 		}
+
+		private void treeView_Folder_MouseUp(object sender, MouseButtonEventArgs e)
+		{
+
+		}
+
+		private void treeView_Folder_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+
+		}
 	}
+
+	public class Folder_data : ObservableCollection<Folder_data>
+	{
+		public Folder_data()
+		{
+			//fd_Children = new Folder_data();
+		}
+
+		public Folder_data(string path)
+		{
+			str_FPath   = path;
+			fd_Children = new Folder_data();
+		}
+
+		public BitmapImage image_Icon  { get; set; }
+		public string      str_FPath   { get; set; }
+		public string      str_FName   { get; set; }
+		public Folder_data fd_Children { get; set; }
+	}
+
+	/*public class Folder_data
+	{
+		public BitmapImage image_Icon { get; set; }
+		public string      str_FFPath { get; set; }
+		public string      str_FFName { get; set; }
+	}*/
 
 	public class FolderFile_data
 	{
